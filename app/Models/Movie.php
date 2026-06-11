bff185a1f79a6b3e85f08cd65a2d0a3038ad5eb8
wabemusic <jvwatanabe@gmail.com>
2026-06-11 18:28:18 -0300

    feat: add Movie model with fillable attributes

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+   protected $fillable = [
+    'title',
+    'director',
+    'genre',
+    'release_year',
+    'rating',
+    'synopsis'
+   ];
 }
