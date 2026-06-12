38d763b5a7e7f0742c5e10f06c31e12e4471b87f
wabemusic <jvwatanabe@gmail.com>
2026-06-11 21:51:58 -0300

    feat: Movie Resource

--- app/Http/Resources/MovieResource.php
+++ app/Http/Resources/MovieResource.php
@@ -7,13 +7,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class MovieResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+           'id' => $this->id,
+            'title' => $this->title,
+            'director' => $this->director,
+            'genre' => $this->genre,
+            'release_year' => $this->release_year,
+            'rating' => $this->rating,
+            'synopsis' => $this->synopsis,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
